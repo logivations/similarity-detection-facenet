306fc5d6fe24bf057344483a52210cb7c47dff91
Karthikeya Kaushik <Karthikeya.kaushik@logivations.com>
2019-02-26 12:01:00 +0100

    Added reference testing and documentation

--- src/facenet.py
+++ src/facenet.py
@@ -211,11 +211,9 @@ def train(total_loss, global_step, optimizer, learning_rate, moving_average_deca
     return train_op
 
 def prewhiten(x):
-    print(x.shape)
     mean = np.mean(x)
     std = np.std(x)
     std_adj = np.maximum(std, 1.0/np.sqrt(x.size))
-    print(x.size)
     y = np.multiply(np.subtract(x, mean), 1/std_adj)
     return y  
 
